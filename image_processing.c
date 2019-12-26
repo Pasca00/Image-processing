b5a9a58aab27c9cd7d3d95d7eaece45b60f4025a
Pasca00 <56002233+Pasca00@users.noreply.github.com>
2019-12-26 23:08:03 +0200

    Add files via upload

--- image_processing.c
+++ image_processing.c
@@ -29,31 +29,6 @@ void black_and_white(unsigned char *bmp_pixel_array, int image_nr, bmp_fileheade
 }
 
 int main(){
-	//bmp_fileheader image1_file_header;
-	//bmp_infoheader image1_info_header;
-	//FILE *image = fopen("input/images/test8.bmp", "rb");
-	//FILE *image_after = fopen("image_after.bmp", "wb");
-	/*
-	fread(&image1_file_header, sizeof(bmp_fileheader), 1, image);
-	fread(&image1_info_header, sizeof(bmp_infoheader), 1, image);
-	unsigned char *bitmapImage = (unsigned char*)malloc(image1_file_header.bfSize);
-	
-	fseek(image, image1_file_header.imageDataOffset, SEEK_SET);
-	fread(bitmapImage, sizeof(unsigned char), image1_file_header.bfSize, image);
-	
-	for(int i = 0; i < image1_file_header.bfSize; i += 3){
-		int avg = (bitmapImage[i] + bitmapImage[i + 1] + bitmapImage[i + 2]) / 3;
-		bitmapImage[i] = avg;
-		bitmapImage[i + 1] = avg;
-		bitmapImage[i + 2] =avg;
-	}
-	
-	fwrite(&image1_file_header, sizeof(bmp_fileheader), 1, image_after);
-	fwrite(&image1_info_header, sizeof(bmp_infoheader), 1, image_after);
-	fwrite(bitmapImage, sizeof(unsigned char*), image1_file_header.bfSize, image_after);
-	
-	printf("%d %d %d\n", bitmapImage[0], bitmapImage[1], bitmapImage[2]);
-	*/
 	char *filename_format = "input/images/test%d.bmp";
 	char *filename = malloc(25 * sizeof(char));
 	
@@ -65,11 +40,10 @@ int main(){
 		FILE *bmp_image = fopen(filename, "rb");
 		
 		load_bmp_header_and_info(&img_header, &img_info, bmp_image);
-		//
-		printf("%u\n", img_header.imageDataOffset);
-		//
+		
 		unsigned char *bmp_pixel_array = (unsigned char*)malloc(img_header.bfSize);
 		fseek(bmp_image, img_header.imageDataOffset, SEEK_SET);
+		fread(bmp_pixel_array, sizeof(unsigned char), img_header.bfSize, bmp_image);
 		
 		black_and_white(bmp_pixel_array, img_nr, img_header, img_info);
 		
